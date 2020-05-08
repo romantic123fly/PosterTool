3dcbcbd58e7bddc6b43d4259e0c1dd7c3a2fee3b
romantic123fly <853394528@qq.com>
2020-05-08 19:08:47 +0800

    no message

--- Assets/PostersTools/DragUI.cs
+++ Assets/PostersTools/DragUI.cs
@@ -18,6 +18,7 @@ public class DragUI : MonoBehaviour, IDragHandler
 
         if (!RectTransformUtility.ScreenPointToLocalPointInRectangle(transform.parent.gameObject.GetComponent<RectTransform>(), eventData.position, Camera.main, out position))
         {
+			
             return;
         }
         transform.localPosition = position;
